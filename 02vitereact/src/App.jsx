2e85178937ea810d156d945bf1b18a11391f5a4a
satyam-trimale <trimalesatyam@gmail.com>
2025-02-23 21:19:03 +0530

    Integrate Tailwind CSS for styling and add Card component to display user information

--- 02vitereact/src/App.jsx
+++ 02vitereact/src/App.jsx
@@ -9,7 +9,20 @@ const [counter,setCounter] = useState(0)
 const addValue = () => {
   if(counter<20){
     setCounter(counter+1)
+    // setCounter(prevCounter => prevCounter + 1)
+    // setCounter(prevCounter => prevCounter + 1 )
+    // setCounter(prevCounter => prevCounter + 1)
+    // setCounter(prevCounter => prevCounter + 1)
 
+    /*
+    Understanding setCounter Behavior
+setCounter updates the counter asynchronously.
+If you directly do setCounter(counter + 1), React might batch the updates and apply only the last one in the same render cycle.
+However, using functional updates (prevCounter => prevCounter + 1), React ensures that every update is based on the latest state.
+
+Batching Behavior in React 🔄
+→ Direct state updates in the same cycle might collapse into one unless functional updates are used.
+    */
   }
 }
 const removeValue = () => {
