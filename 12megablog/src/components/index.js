e7de56e8ba295f918a3f4fd3d87d5e9ec64d27c3
satyam-trimale <trimalesatyam@gmail.com>
2025-03-31 23:44:46 +0530

    Add authentication pages and components; implement rich text editor for post content

--- 12megablog/src/components/index.js
+++ 12megablog/src/components/index.js
@@ -3,11 +3,26 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import Signup from "./Signup"
+import Login from "./Login"
+import PostForm from "./post-form/PostForm";
+import PostCard from './PostCard'
+import AuthLayout from "./AuthLayout"
+
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input,
+    Signup,
+    Login,
+    PostCard,
+    PostForm,
+    
 }
